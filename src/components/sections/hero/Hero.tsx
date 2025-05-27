caf5789d7d0b8d2e1dfbdaa7122745125d23521f
Arciiix <artur.nowak100@gmail.com>
2025-05-27 17:46:54 +0200

    Fix invalid anchor

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -63,7 +63,7 @@ export default function Hero() {
                 transition={{ duration: 0.8, delay: 3.5 }}
               >
                 <a
-                  href="#"
+                  href="#getStarted"
                   className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                 >
                   <span className="relative text-base font-semibold text-white">
